1c0d150ed7cb20c8555f0048c691b7a4fa7a257b
RudhraManjula <68630473+RudhraManjula@users.noreply.github.com>
2020-07-31 18:22:57 +0530

    2nd commit

--- Instance.bat
+++ Instance.bat
@@ -1,1 +1,1 @@
-aws ec2 run-instances --image-id ami-081afb283b98a8548 --count 1 --instance-type t2.micro --key-name Manjulakey --security-groups MySecurityGroup --region us-east-1
+aws ec2 run-instances --image-id ami-09d8b5222f2b93bf0 --count 1 --instance-type t2.micro --key-name Manjulakey --security-groups MySecurityGroup --region us-east-1

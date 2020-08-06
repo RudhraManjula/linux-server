1ea6cf99f6c06b1c5526e77ce127afd598506105
RudhraManjula <68630473+RudhraManjula@users.noreply.github.com>
2020-08-06 09:54:11 +0530

    Update Instance.bat

--- Instance.bat
+++ Instance.bat
@@ -1,1 +1,1 @@
-aws ec2 run-instances --image-id ami-067f5c3d5a99edc80 --count 2 --instance-type t2.micro --key-name Manjulakey --security-groups MySecurityGroup --region us-east-1
+aws ec2 run-instances --image-id ami-09d8b5222f2b93bf0 --count 2 --instance-type t2.micro --key-name Manjulakey --security-groups MySecurityGroup --region us-east-1

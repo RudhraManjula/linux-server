218271e4d35fa20ede9d527e036ae6896249a9f4
RudhraManjula <68630473+RudhraManjula@users.noreply.github.com>
2020-08-06 09:49:14 +0530

    Update SecurityGroup.bat

--- SecurityGroup.bat
+++ SecurityGroup.bat
@@ -1,1 +1,1 @@
-aws ec2 create-security-group --group-name MySecurityGroup --description "MySecurityGroup" --region us-west-2
+aws ec2 create-security-group --group-name MySecurityGroup --description "MySecurityGroup" --region us-east-1
